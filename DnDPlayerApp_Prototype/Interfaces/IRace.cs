5bbff46f900b16fc8a045be55de70a2dcbab939c
kana4y4a <kana4y4a@gmail.com>
2019-06-24 21:16:51 +0300

    IRace implementation

--- DnDPlayerApp_Prototype/Interfaces/IRace.cs
+++ DnDPlayerApp_Prototype/Interfaces/IRace.cs
@@ -1,6 +1,19 @@
 ﻿namespace DnDPlayerApp_Prototype.Interfaces
 {
+    using System.Collections.Generic;
+    using DnDPlayerApp_Prototype.Models.Emuns;
+
     public interface IRace
     {
+        IRace SubRace { get; }
+
+        int Age { get; }
+        IAlignment Alignment { get; }
+        int Size { get; }
+        int Speed { get; }
+        IList<IWeapon> CombatTraining { get;}
+
+        int GetAbilityScoreIncrease(Stat stat);
+        int GetSkillScoreIncrease(Skill skill);
     }
 }
